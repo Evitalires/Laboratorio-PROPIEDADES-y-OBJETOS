c3956751fc3fcfa6ef792817fd1ea76d1d7066f5
Evitalires <evitalires@gmail.com>
2025-10-31 11:58:49 -0500

    Resolviendo ejercicio 3

--- index.js
+++ index.js
@@ -44,5 +44,50 @@ console.log(listaCompra1.agregar("Condominio"));
 console.log("Lista de compra: ", listaCompra1.productos);
 console.log("Lista de compras numerada: ", listaCompra1.mostrar());
 
+// Ejercicio 3: Reproductor de Música
+function Reproductor() {
+    this.estado = "detenido";
+    this.play = function () {
+        if(this.estado == "detenido" || "pausado" ) {
+            this.estado = "reproduciendo";
+            console.log("Reproduciendo música");
+        } 
+        else if (this.estado == "reproduciendo"){
+            console.log("Ya se está reproduciendo música. ");
+        }
+    }
+    this.pausar = function () {
+         if(this.estado == "reproduciendo") {
+            this.estado = "pausado";
+            console.log("Música pausada.");
+        } 
+        else {
+            console.log("No hay música reproduciéndose.");
+        }
+    }
+    this.detener = function () {
+        if (this.estado == "pausado") {
+            console.log("No hay música reproduciéndose.");
+            this.estado = "detenido";
+
+        } else {
+            console.log("Música Detenida.");
+        }
+    }
+}
+
+const reproductor = new Reproductor();
+console.log("Reproductor");
+console.log(reproductor.estado);
+console.log(reproductor.play());
+console.log(reproductor.pausar());
+console.log(reproductor.play());
+console.log(reproductor.detener());
+console.log(reproductor.pausar());
+console.log(reproductor.pausar());
+
+
+
+
 
 
