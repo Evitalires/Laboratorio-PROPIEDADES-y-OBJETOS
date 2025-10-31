072ce14b809ca33e7d813806d3d99f8c2bd7975a
Evitalires <evitalires@gmail.com>
2025-10-31 11:34:14 -0500

    Resolviendo ejercicio 2

--- index.js
+++ index.js
@@ -21,3 +21,28 @@ console.log(ensayoCeguera.prestado);
 console.log(ensayoCeguera.prestar());
 
 
+//Ejercicio 2: Lista de Compras (Array + for)
+
+function ListaCompras() {
+    this.productos = [];
+    this.agregar = function(producto) {
+        console.log(typeof producto);
+        this.productos.push(producto);
+    };
+    this.mostrar = function(){
+        for (let i = 0; i < this.productos.length; i++) {
+            const element = this.productos[i];
+            console.log(i+1, ". ", element);
+        }
+    }
+}
+const listaCompra1 = new ListaCompras();
+console.log("Lista de compra: ", listaCompra1.productos);
+console.log(listaCompra1.agregar("Televisor"));
+console.log(listaCompra1.agregar("Patineta"));
+console.log(listaCompra1.agregar("Condominio"));
+console.log("Lista de compra: ", listaCompra1.productos);
+console.log("Lista de compras numerada: ", listaCompra1.mostrar());
+
+
+
